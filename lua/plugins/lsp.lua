d93ccc9397b111cf98a1ac138e1844e3363fb9c8
ClemGamer <tsaiyouchen@gmail.com>
2025-02-03 23:59:05 +0800

    Setup ts_ls and eslint

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -252,6 +252,21 @@ return {
 					},
 				},
 			})
+
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.eslint.setup({
+				capabilities = capabilities,
+				on_attach = function(client, bufnr)
+					print("attach eslint")
+					-- vim.api.nvim_create_autocmd("BufWritePre", {
+					-- 	buffer = bufnr,
+					-- 	command = "EslintFixAll",
+					-- })
+				end,
+			})
 		end,
 	},
 }
